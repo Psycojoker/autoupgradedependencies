9478a698888d6e33bdbc3c6c9afe31aef9a14812
Laurent Peuch <cortex@worlddomination.be>
2019-04-23 20:37:15 +0200

    [enh] get list of possibles upgrades for a package

--- autoupgradedependencies.py
+++ autoupgradedependencies.py
@@ -1,7 +1,13 @@
 # encoding: utf-8
 
 import os
+import re
 import sys
+import operator
+import itertools
+
+from distutils.version import LooseVersion
+
 import requests
 
 from redbaron import RedBaron
@@ -13,10 +19,15 @@ def find_pkginfo(path):
     if "__pkginfo__.py" in dirs:
         return os.path.join(path, "__pkginfo__.py")
 
-    cube_subdir = [os.path.join(path, x) for x in dirs if os.path.isdir(x) and x.startswith("cubicweb_")]
+    cube_subdirs = [os.path.join(path, x) for x in dirs if os.path.isdir(x) and x.startswith("cubicweb_")]
 
-    if cube_subdir and "__pkginfo__.py" in os.listdir(cube_subdir[0]):
-        return os.path.join(cube_subdir[0], "__pkginfo__.py")
+    if not cube_subdirs:
+        print("Couldn't find the __pkginfo__.py file :(")
+        sys.exit(1)
+
+    for subdir in cube_subdirs:
+        if "__pkginfo__.py" in os.listdir(subdir):
+            return os.path.join(subdir, "__pkginfo__.py")
 
     print("Couldn't find the __pkginfo__.py file :(")
     sys.exit(1)
@@ -48,10 +59,64 @@ def merge_depends_with_pypi_info(depends):
         new_depends[key] = {
             "pkg_name": pkg_name,
             "current_version_scheme": value,
-            "all_versions": data["releases"].keys()
+            "all_versions": data["releases"].keys(),
         }
 
     return new_depends
+
+
+def filter_pkg_that_can_be_upgraded(depends):
+    new_depends = {}
+    for key, value in depends.items():
+        conditions = parse_conditions(value["current_version_scheme"])
+
+        if conditions is None:
+            print("No specified version for %s, drop it" % key)
+            continue
+
+        compatible_versions = value["all_versions"]
+
+        for (op, version) in conditions:
+            compatible_versions = [x for x in compatible_versions if op(LooseVersion(x), LooseVersion(version))]
+
+        maximum_version = list(sorted(map(LooseVersion, compatible_versions)))[-1]
+        all_versions_sorted = sorted(map(LooseVersion, value["all_versions"]))
+        possible_upgrades = list(itertools.dropwhile(lambda x: x < maximum_version, all_versions_sorted))
+
+        if possible_upgrades:
+            print("%s can be upgraded to %s" % (key, ", ".join([x.vstring for x in possible_upgrades])))
+            new_depends[key] = value
+            new_depends[key]["possible_upgrades"] = possible_upgrades
+        else:
+            print("No possible upgrades for %s, drop it" % key)
+
+    return new_depends
+
+
+def parse_conditions(conditions):
+    string_to_operator = {
+        "==": operator.eq,
+        "<": operator.lt,
+        "<=": operator.le,
+        "!=": operator.ne,
+        ">=": operator.ge,
+        ">": operator.gt,
+    }
+
+    parsed_conditions = []
+
+    if not conditions:
+        return None
+
+    for i in conditions.split(","):
+        version_operator, version_number = re.match("(==|>=|<=|>|<) *([0-9.]*)", i.strip()).groups()
+
+        parsed_conditions.append([
+            string_to_operator[version_operator],
+            version_number,
+        ])
+
+    return parsed_conditions
 
 
 def main():
@@ -64,13 +129,21 @@ def main():
     depends = parse_pkginfo(pkginfo_path)
     cubes = [x for x in depends if x.startswith("cubicweb-")]
 
+    print("")
+
     if cubes:
         sys.stdout.write("Found cubes:\n* ")
         print("\n* ".join(cubes))
     else:
         print("This cube doesn't depends on other cubes")
 
+    print("")
+
     depends = merge_depends_with_pypi_info(depends)
+
+    print("")
+
+    depends = filter_pkg_that_can_be_upgraded(depends)
     print(depends)
 
 
