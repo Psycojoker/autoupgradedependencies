16e8fc969b23d86d1fed83c4fc325eea76dfdb2c
Laurent Peuch <cortex@worlddomination.be>
2019-04-23 18:19:38 +0200

    [enh] parse pkginfo

--- autoupgradedependencies.py
+++ autoupgradedependencies.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from redbaron import RedBaron
+
 
 def find_pkginfo(path):
     dirs = os.listdir(path)
@@ -17,11 +19,37 @@ def find_pkginfo(path):
     sys.exit(1)
 
 
+def parse_pkginfo(path):
+    red = RedBaron(open(path, "r").read())
+
+    depends = red.find("assign", lambda x: x.target.value == "__depends__")
+
+    if not depends:
+        print("I couldn't find __depends__ in the __pkginfo__.py :(")
+        sys.exit(1)
+
+    assert depends.value.type == "dict"
+
+    # oupsi I'm lazy, it's bad :(
+    # XXX use to_python() somehow
+    return eval(depends.value.dumps())
+
+
 def main():
     path = "."
     path = os.path.realpath(os.path.expanduser(path))
 
     pkginfo_path = find_pkginfo(path)
+    print("Foudn __pkginfo__.py: %s" % pkginfo_path)
+
+    depends = parse_pkginfo(pkginfo_path)
+    cubes = [x for x in depends if x.startswith("cubicweb-")]
+
+    if cubes:
+        sys.stdout.write("Found cubes:\n* ")
+        print("\n* ".join(cubes))
+    else:
+        print("This cube doesn't depends on other cubes")
 
 
 if __name__ == '__main__':
